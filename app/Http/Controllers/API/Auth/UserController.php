c5937bd44af24a80dd60d298141df0f46e18edce
GraceKiarie <gnjokikiarie@gmail.com>
2019-11-09 09:22:54 +0300

    updates

--- app/Http/Controllers/API/Auth/UserController.php
+++ app/Http/Controllers/API/Auth/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
-use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
