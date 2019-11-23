57d83d20e5f268fe9b2a038cbd73c00c0c161e5e
GraceKiarie <gnjokikiarie@gmail.com>
2019-11-23 12:10:54 +0300

    updates

--- app/Http/Controllers/API/TestApp/TestReportController.php
+++ app/Http/Controllers/API/TestApp/TestReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\TestApp;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SaveTestReportJob;
 use App\User;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade as PDF;
@@ -22,7 +23,6 @@ class TestReportController extends Controller
 
     }
 
-
     public function sendMail(Request $request, $id )
     {
 
@@ -33,11 +33,11 @@ class TestReportController extends Controller
             'voltage' => $request->get('voltage'),
             'csq' => $request->get('csq'),
             'name' => $user->name,
+            'user_id' => $user->id,
             'email' => $user->email,
             'role' => $user->role->role_name,
         ];
-
-
+        
         if ($data['voltage'] >4.8){
             $data['voltage_test'] = 'Passed';
         }else{
@@ -59,17 +59,20 @@ class TestReportController extends Controller
         $data['date']=date("Y-m-d",$t);
         $data['time']=date('H:i:s',$t);
 
+        //generate pdf and save to directory
 
         $pdf = PDF::loadView('mails.test_report', compact('data'));
-
-        // save mail to directory
         $filename = $data['name']."_".$data['qr_number'] . '_testReport.pdf';
-
         Storage::put('public/testReport/' . $filename, $pdf->output());
-
         $uri = '/home/kiarie/Desktop/Antenna-Monitor/storage/app/public/testReport/' . $filename;
+
         Log::info("Request cycle without Queues started");
 
+        //save report info to db
+        $savetodbJob = (new SaveTestReportJob($data))->delay(Carbon::now()->addSeconds(2));
+        dispatch($savetodbJob );
+
+        //send report to email
         $emailJob = (new SendTestReportEmailJob($uri))->delay(Carbon::now()->addSeconds(3));
         dispatch($emailJob);
 
