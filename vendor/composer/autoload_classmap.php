6dea1ce535b9b78bbc0e4d5f6488c2c1e0759657
benodhiambo <benodhimabo@mail.com>
2019-11-25 08:33:12 +0300

    Colour changes to UI - Update autoload_namespaces.php and installed.json

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -51,6 +51,7 @@ return array(
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
     'App\\InstallationReport' => $baseDir . '/app/InstallationReport.php',
+    'App\\Jobs\\SaveTestReportJob' => $baseDir . '/app/Jobs/SaveTestReportJob.php',
     'App\\Jobs\\SendTestReportEmailJob' => $baseDir . '/app/Jobs/SendTestReportEmailJob.php',
     'App\\Mail\\TestReportEmail' => $baseDir . '/app/Mail/TestReportEmail.php',
     'App\\Monitor' => $baseDir . '/app/Monitor.php',
